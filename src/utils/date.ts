66e7faf58b0ef881300e32ea560e7726d76f02b4
Jose Lezama <me@joseglego.io>
2023-08-09 07:44:53 -0400

    core(utils.date): handle nulish values

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,8 @@
 import { isMonday, isSunday, previousMonday, nextSunday, isBefore, isAfter  } from 'date-fns'
 
-export const currentWeek = (_date: string) => {
-  const date = new Date(_date);
+type PossibleDate = string | null | undefined
+export const currentWeek = (_date: PossibleDate) => {
+  const date = new Date(_date || '');
   const today = new Date();
   const monday =  isMonday(today) ? today : previousMonday(today);
   const sunday = isSunday(today) ? today : nextSunday(today);
@@ -9,32 +10,32 @@ export const currentWeek = (_date: string) => {
   return isAfter(date, monday) && isBefore(date, sunday);
 }
 
-export const afterCurrentWeek = (_date: string) => {
-  const date = new Date(_date);
+export const afterCurrentWeek = (_date: PossibleDate) => {
+  const date = new Date(_date || '');
   const today = new Date();
   const sunday = isSunday(today) ? today : nextSunday(today);
 
   return isAfter(date, sunday);
 }
 
-export const beforeCurrentWeek = (_date: string) => {
-  const date = new Date(_date);
+export const beforeCurrentWeek = (_date: PossibleDate) => {
+  const date = new Date(_date || '');
   const today = new Date();
   const monday =  isMonday(today) ? today : previousMonday(today);
 
   return isBefore(date, monday);
 }
 
-export const sortByDateAsc = (_dateA: string, _dateB: string) => {
-  const dateA = new Date(_dateA)
-  const dateB = new Date(_dateB)
+export const sortByDateAsc = (_dateA: PossibleDate, _dateB: PossibleDate) => {
+  const dateA = new Date(_dateA || '')
+  const dateB = new Date(_dateB || '')
 
   return dateA <= dateB ? -1 : 1
 }
 
-export const sortByDateDesc = (_dateA: string, _dateB: string) => {
-  const dateA = new Date(_dateA)
-  const dateB = new Date(_dateB)
+export const sortByDateDesc = (_dateA: PossibleDate, _dateB: PossibleDate) => {
+  const dateA = new Date(_dateA || '')
+  const dateB = new Date(_dateB || '')
 
   return dateA > dateB ? -1 : 1
 }
