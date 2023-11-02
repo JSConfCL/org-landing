09c569f7f86ad36a34cd6a377f06015e2802b02d
Felipe Torres <felipe.torressepulveda@gmail.com>
2023-11-02 14:55:54 -0700

    Adding new model

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -2,9 +2,10 @@ import { type SchemaTypeDefinition } from "sanity";
 
 import status from "./schemas/status";
 import donor from "./schemas/donor";
+import eventImage from "./schemas/eventImage";
 import eventInstance from "./schemas/eventInstance";
 import eventType from "./schemas/eventType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [eventType, eventInstance, status, donor],
+  types: [eventType, eventInstance, status, donor, eventImage],
 };
