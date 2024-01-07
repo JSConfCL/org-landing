42e9e515f09f4d7ee330596f9d51e2d01f5d4835
Jose Lezama <me@joseglego.io>
2024-01-07 18:51:51 -0300

    TMP core: update reference to new sanity shcema

--- graphql.config.ts
+++ graphql.config.ts
@@ -1,7 +1,7 @@
 import type { IGraphQLConfig } from "graphql-config";
 
 const projectId = "t2zgeg0i"
-const dataset = "production"
+const dataset = "migration"
 
 const config: IGraphQLConfig = {
   schema: `https://${projectId}.api.sanity.io/v1/graphql/${dataset}/default`,
