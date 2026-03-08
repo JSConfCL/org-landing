7e1f0e78e4558d027665ceb555690774c4493a7d
Fabián <me@fabianko.com>
2026-03-08 15:37:31 -0300

    Migrate next.config.ts to CommonJS module syntax
    Signed-off-by: Fabián <me@fabianko.com>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true
 };
 
-export default nextConfig;
+module.exports = nextConfig;
